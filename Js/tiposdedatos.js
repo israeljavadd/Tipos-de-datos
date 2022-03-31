708940d060f1dfd85320593abfc656f3b4dc7985
Israeljavadd <israeljavadd@gmail.com>
2022-03-30 18:22:23 -0600

    Tipos de datos

---
+++ Js/tiposdedatos.js
@@ -0,0 +1,16 @@
+
+    var string="Hola"; //Tipo string
+    var int=5; //Tipo int
+    var float=2.5; //Tipo Float
+    var boolean=true; //Tipo boleano
+    var undefined; //Undefined
+
+    document.write("   '"+string + "' Variable de Tipo Indefinida  //");
+    document.write("<br>");
+    document.write("   '"+int + "' Variable de Tipo Float //");
+    document.write("<br>");
+    document.write("   '"+float + "' Variable de Tipo Booleana  //");
+    document.write("<br>");
+    document.write("   '"+boolean + "' Variable de Tipo  Int  //");
+    document.write("<br>");
+    document.write("   '"+undefined + "' Variable de Tipo String //");
